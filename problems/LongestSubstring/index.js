35a02c085a7048c395f741dec9531260dffcb348
Rocky Singh <rockygsingh@icloud.com>
2019-09-04 23:03:12 -0400

    commented out the code for readablity

--- problems/LongestSubstring/index.js
+++ problems/LongestSubstring/index.js
@@ -7,13 +7,19 @@ function lengthOfLongestSubstring(s) {
     let maxLength = 0
 
     for(let i = 0 ; i< s.length; i++){
+        // current char 
         const endChar = s[i];
 
+        // if the chars value is greater then windowStart then 
+        // of already passed it in the window need to start a new windw
         if(windowCharMap[endChar] >= windowStart){
             windowStart = windowCharMap[endChar] + 1;
         }
 
+        // update the map with right char and index
         windowCharMap[endChar] = i;
+
+        // update the max length of the winddow
         maxLength = Math.max(maxLength, i - windowStart + 1);
     }
 
