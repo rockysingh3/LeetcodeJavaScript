911f036a4a8df4e88e3e5500b1b42fd16fccd06d
Rocky Singh <rockygsingh@icloud.com>
2019-09-06 23:20:01 -0400

    isAnagram

---
+++ problems/isAnagram/index.js
@@ -0,0 +1,29 @@
+function isAnagram(s, t) {
+
+    if(s.length !== t.length) {
+        return false;
+    }
+
+    const sCharCounts = {} // {H: 1, E: 1, L: 2, 0: 1}
+
+    // Fill sCharCounts
+    for(let i=0; i<s.length; i++){
+        const sChar = s[i]
+        sCharCounts[sChar] = sCharCounts[sChar]
+    }
+
+    for (let i=0; i<t.length; i++) {
+        const tChar = t[i]
+
+        if(!sCharCounts[tChar]) {
+            return false
+        } else {
+            sCharCounts[tChar]--
+        }
+    }
+
+    return true
+
+}
+
+module.exports = isAnagram
