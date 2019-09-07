3f0ca2d497945782b6b3a1c553070f358dc677a8
Rocky Singh <rockygsingh@icloud.com>
2019-09-06 23:07:18 -0400

    groupAngrams

---
+++ problems/groupAnagrams/index.js
@@ -0,0 +1,24 @@
+// Problem number 49 on Leetcode
+
+function  groupAnagrams(strs) {
+
+    let grouped = {}
+
+    for (let i=0; i<strs.length; i++){
+        const word = strs[i]
+        const key = word
+            .split('')
+            .sort()
+            .join("")
+
+        if(!grouped[key]) {
+            grouped[key] = []
+        }
+
+        grouped[key].push(word)
+    }
+
+    return Object.values(grouped)
+}
+
+module.exports = groupAnagrams;
